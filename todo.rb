fb2313baff437aacd0e5643542565250980f07de
Rebecca Biancofiore <rebeccabiancofiore79@gmail.comm>
2023-08-24 15:32:17 -0400

    sinatra todo files lessons 1-6

---
+++ todo.rb
@@ -0,0 +1,28 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do 
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do 
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do 
+  session[:lists] << { name: "New List", todos: [] }
+ # redirect "/lists"
+end
+
+set :session_secret, SecureRandom.hex(32)
