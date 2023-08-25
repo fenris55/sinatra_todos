1c4bfe8145c5eeb9a5f17b4bf14a072cf1a1066f
Rebecca Biancofiore <rebeccabiancofiore79@gmail.comm>
2023-08-25 09:39:47 -0400

    update project files

--- todo.rb
+++ todo.rb
@@ -2,12 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
-configure do 
+configure do
   enable :sessions
   set :session_secret, 'secret'
 end
 
-before do 
+before do
   session[:lists] ||= []
 end
 
@@ -15,14 +15,39 @@ get "/" do
   redirect "/lists"
 end
 
+# get all lists
 get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
-get "/lists/new" do 
-  session[:lists] << { name: "New List", todos: [] }
- # redirect "/lists"
+# render new list form
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+# return error message if name is invalid, return nil if name is valid
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
+# create new list
+post "/lists" do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect "/lists"
+  end
 end
 
 set :session_secret, SecureRandom.hex(32)
